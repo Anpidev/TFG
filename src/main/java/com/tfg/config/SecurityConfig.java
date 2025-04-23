9675fd2051ffeb4789be0e52647c9cc1f4b7e028
Anpidev <angelaruizdev@gmail.com>
2025-04-23 17:55:15 +0200

    cors hacia front en netlify (nombre dominio cambiado)

--- src/main/java/com/tfg/config/SecurityConfig.java
+++ src/main/java/com/tfg/config/SecurityConfig.java
@@ -56,7 +56,7 @@ public class SecurityConfig {
     CorsConfigurationSource corsConfigurationSource(){
         CorsConfiguration configuration = new CorsConfiguration();
 
-        configuration.setAllowedOrigins(List.of("https://gleeful-puffpuff-762e75.netlify.app"));
+        configuration.setAllowedOrigins(List.of("https://analitica-express.netlify.app"));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("Authorization", "Content-Type"));
         configuration.setAllowCredentials(true);
